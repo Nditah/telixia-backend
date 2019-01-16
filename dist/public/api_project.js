894db5bbc9c549a6e53a970d61e0a02bc1ed451d
Nditah <nditah@gmail.com>
2019-01-16 11:53:50 +0100

    Updated Doc for Bank, Package Module

--- dist/public/api_project.js
+++ dist/public/api_project.js
@@ -7,7 +7,7 @@ define({
   "defaultVersion": "0.0.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-13T12:12:55.238Z",
+    "time": "2019-01-16T10:52:16.514Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
